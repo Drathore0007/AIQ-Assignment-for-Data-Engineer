3a9a7db03950b754c5fc4db52e1817f83e90bf64
Dharmendra kumar <drathore586@gmail.com>
2024-03-24 14:09:39 +0530

    Update Gettoken.py

--- code/AIQSales/Gettoken.py
+++ code/AIQSales/Gettoken.py
@@ -2,7 +2,7 @@ import requests
 
 class AIQ_GetAuth:
     def __init__(self):
-        self.access_token = '6fa5e6746950f40eb84866daf2b3ef45'
+        self.access_token = '6fa5e6746950f40eb84866daf2b3ef50'
 
 if __name__ == "__main__":
     token =  AIQ_GetAuth()
